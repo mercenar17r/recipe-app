6f0eab852f67458560fb67cf2c2febc0b876f1a9
Cosmin Negrut <cosminnegrut2002@gmai.com>
2022-07-22 22:06:28 +0300

    add guard clause

--- src/js/controller.js
+++ src/js/controller.js
@@ -30,6 +30,7 @@ const showRecipe = async function () {
   try {
     const id = window.location.hash.slice(1);
     console.log(id);
+    if (!id) return;
 
     //1.Loading the recipe
     renderSpinner(recipeContainer);

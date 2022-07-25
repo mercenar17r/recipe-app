76df445bb1ec5fc2c86790a81485ac809ebd2035
Cosmin Negrut <cosminnegrut2002@gmail.com>
2022-07-25 23:55:26 +0300

    refactor ing

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -2403,20 +2403,7 @@ class RecipeView {
   <div class="recipe__ingredients">
     <h2 class="heading--2">Recipe ingredients</h2>
     <ul class="recipe__ingredient-list">
-    ${this.#data.ingredients.map((ing)=>{
-            return `
-          <li class="recipe__ingredient">
-          <svg class="recipe__icon">
-            <use href="${_iconsSvgDefault.default}#icon-check"></use>
-          </svg>
-          <div class="recipe__quantity">${new _fractional.Fraction(ing.quantity).toString()}</div>
-          <div class="recipe__description">
-            <span class="recipe__unit">${ing.unit}</span>
-            ${ing.description}
-          </div>
-        </li>
-     `;
-        }).join('')}
+    ${this.#data.ingredients.map(this.#generateMarkupIngredient).join('')}
 </div>
 
   <div class="recipe__directions">
@@ -2438,6 +2425,20 @@ class RecipeView {
     </a>
   </div>
 `;
+    }
+     #generateMarkupIngredient(ing) {
+        return `
+      <li class="recipe__ingredient">
+      <svg class="recipe__icon">
+        <use href="${_iconsSvgDefault.default}#icon-check"></use>
+      </svg>
+      <div class="recipe__quantity">${ing.quantity ? new _fractional.Fraction(ing.quantity).toString() : ''}</div>
+      <div class="recipe__description">
+        <span class="recipe__unit">${ing.unit}</span>
+        ${ing.description}
+      </div>
+    </li>
+ `;
     }
 }
 exports.default = new RecipeView();

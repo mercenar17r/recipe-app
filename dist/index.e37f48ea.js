4751a3684d7f10df10006547886eab5df75669d0
Cosmin Negrut <cosminnegrut2002@gmail.com>
2022-07-31 13:08:06 +0300

    add servings limit

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -2460,9 +2460,8 @@ class RecipeView extends _viewJsDefault.default {
         this._parentElement.addEventListener('click', function(e) {
             const btn = e.target.closest('.btn--update-servings');
             if (!btn) return;
-            console.log(btn);
-            const updateTo = +btn.dataset.updateTo;
-            handler(updateTo);
+            const { updateTo  } = btn.dataset;
+            if (+updateTo > 0) handler(+updateTo);
         });
     }
     _generateMarkup() {
